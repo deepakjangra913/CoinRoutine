488c3255eaba86a8b5ac8ef6c131c5b3002dd4b9
deepakjangra913 <Deepakjangra605@gmail.com>
2026-04-20 05:08:51 +0530

    Commit includes - Setup colors - Setup colors for dark and light theme

--- composeApp/src/commonMain/kotlin/com/deepak/coinroutine/App.kt
+++ composeApp/src/commonMain/kotlin/com/deepak/coinroutine/App.kt
@@ -1,10 +1,13 @@
 package com.deepak.coinroutine
 
 import androidx.compose.runtime.Composable
+import com.deepak.coinroutine.theme.CoinRoutineTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    CoinRoutineTheme {
 
+    }
 }
