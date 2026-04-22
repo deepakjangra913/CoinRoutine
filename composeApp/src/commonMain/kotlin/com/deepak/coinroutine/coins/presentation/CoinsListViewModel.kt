f1cb2d32c8f456fdc6f0cec0f8739b2938e63f8b
deepakjangra913 <Deepakjangra605@gmail.com>
2026-04-22 05:58:33 +0530

    Commit includes - ViewModel added for Coins List Screen

---
+++ composeApp/src/commonMain/kotlin/com/deepak/coinroutine/coins/presentation/CoinsListViewModel.kt
@@ -0,0 +1,57 @@
+package com.deepak.coinroutine.coins.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deepak.coinroutine.coins.domain.GetCoinsListUseCase
+import com.deepak.coinroutine.core.domain.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class CoinsListViewModel(
+    private val getCoinsListUseCase: GetCoinsListUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinsState())
+    val state = _state
+        .onStart {
+            getAllCoins()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = CoinsState()
+        )
+
+    private suspend fun getAllCoins() {
+        when (val coinsResponse = getCoinsListUseCase.execute()) {
+            is Result.Error -> {
+                _state.update {
+                    it.copy(
+                        coins = emptyList(),
+                        error = null
+                    )
+                }
+            }
+
+            is Result.Success -> {
+                _state.update {
+                    CoinsState(
+                        coins = coinsResponse.data.map { coinItem ->
+                            UiCoinListItem(
+                                id = coinItem.coin.id,
+                                name = coinItem.coin.name,
+                                symbol = coinItem.coin.symbol,
+                                iconUrl = coinItem.coin.iconUrl,
+                                formattedPrice = coinItem.price.toString(),
+                                formattedChange = coinItem.change.toString(),
+                                isPositive = coinItem.change >= 0
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
