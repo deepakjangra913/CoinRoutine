a65374667124ed2445d16b13fe34265661001d0f
deepakjangra913 <Deepakjangra605@gmail.com>
2026-04-25 23:49:24 +0530

    Commit includes - fixed Room version issue for ios - Added system bar padding - Made title as sticky header on listing screen

---
+++ composeApp/src/iosMain/kotlin/com/deepak/coinroutine/core/util/Formatter.ios.kt
@@ -0,0 +1,50 @@
+package com.deepak.coinroutine.core.util
+
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumberFormatterDecimalStyle
+
+actual fun formatFiat(amount: Double, showDecimal: Boolean): String {
+    val numberFormatter = NSNumberFormatter()
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle
+
+    when {
+        showDecimal.not() -> {
+            numberFormatter.minimumFractionDigits = 0.toULong()
+            numberFormatter.maximumFractionDigits = 0.toULong()
+        }
+
+        amount >= 0.01 -> {
+            numberFormatter.maximumFractionDigits = 2.toULong()
+            numberFormatter.minimumFractionDigits = 2.toULong()
+        }
+
+        else -> {
+            numberFormatter.minimumFractionDigits = 8.toULong()
+            numberFormatter.maximumFractionDigits = 8.toULong()
+        }
+    }
+
+    val formatterAmount = numberFormatter.stringFromNumber(NSNumber(amount))
+    return if (formatterAmount != null) "$ $formatterAmount" else ""
+}
+
+actual fun formatCoinUnit(amount: Double, symbol: String): String {
+    val numberFormatter = NSNumberFormatter()
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle
+    numberFormatter.minimumFractionDigits = 8.toULong()
+    numberFormatter.maximumFractionDigits = 8.toULong()
+    val prefix = if (amount >= 0) "+" else ""
+
+    return prefix + numberFormatter.stringFromNumber(NSNumber(amount)) + " $symbol"
+}
+
+actual fun formatPercentage(amount: Double): String {
+    val numberFormatter = NSNumberFormatter()
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle
+    numberFormatter.minimumFractionDigits = 2.toULong()
+    numberFormatter.maximumFractionDigits = 2.toULong()
+    val prefix = if (amount >= 0) "+" else ""
+
+    return prefix + numberFormatter.stringFromNumber(NSNumber(amount)) + " %"
+}
