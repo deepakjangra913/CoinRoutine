bc57d3f947b0cbd6015f4ccffb8af473b1ef04c8
deepakjangra913 <Deepakjangra605@gmail.com>
2026-05-03 17:33:40 +0530

    Commit includes - Fixed @ConstructedBy annotation issue in ios build

--- composeApp/src/commonMain/kotlin/com/deepak/coinroutine/database/portfolio/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/deepak/coinroutine/database/portfolio/PortfolioDatabase.kt
@@ -1,5 +1,6 @@
 package com.deepak.coinroutine.database.portfolio
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.deepak.coinroutine.portfolio.data.local.PortfolioCoinEntity
@@ -7,6 +8,7 @@ import com.deepak.coinroutine.portfolio.data.local.PortfolioDao
 import com.deepak.coinroutine.portfolio.data.local.UserBalanceDao
 import com.deepak.coinroutine.portfolio.data.local.UserBalanceEntity
 
+@ConstructedBy(PortfolioDatabaseCreator::class)
 @Database(entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
--- composeApp/src/commonMain/kotlin/com/deepak/coinroutine/database/portfolio/PortfolioDatabaseFactory.kt
+++ composeApp/src/commonMain/kotlin/com/deepak/coinroutine/database/portfolio/PortfolioDatabaseFactory.kt
@@ -1,9 +1,13 @@
 package com.deepak.coinroutine.database.portfolio
 
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object PortfolioDatabaseCreator: RoomDatabaseConstructor<PortfolioDatabase>
 
 fun getPortFolioDatabase(builder: RoomDatabase.Builder<PortfolioDatabase>): PortfolioDatabase{
     return builder
