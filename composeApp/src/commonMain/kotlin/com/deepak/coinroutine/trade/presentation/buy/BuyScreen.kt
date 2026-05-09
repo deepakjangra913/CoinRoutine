2d84db33aad962ea441e3d966416f8ccf44e4ad0
deepakjangra913 <Deepakjangra605@gmail.com>
2026-05-09 19:57:19 +0530

    Commit Includes - Created Buy screen - Passed Different Trade type to differentiate the screen

---
+++ composeApp/src/commonMain/kotlin/com/deepak/coinroutine/trade/presentation/buy/BuyScreen.kt
@@ -0,0 +1,24 @@
+package com.deepak.coinroutine.trade.presentation.buy
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.deepak.coinroutine.trade.presentation.common.TradeScreen
+import com.deepak.coinroutine.trade.presentation.common.TradeType
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun BuyScreen(
+    coinId: String,
+    navigateToPortfolio: () -> Unit
+) {
+    val viewModel = koinViewModel<BuyViewModel>()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    TradeScreen(
+        state = state,
+        tradeType = TradeType.BUY,
+        onAmountChanged = viewModel::onAmountChanged,
+        onSubmitClicked = viewModel::onBuyClicked
+    )
+}
